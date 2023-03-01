e5751edf8bc647d8fea63d5bbda891fabab4bead
morevillo <mlgorevillo@yahoo.com>
2023-03-01 23:14:51 +0000

    Fix version of module

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,9 @@
 module "vpc" {
     source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
+    version = "3.4.0"
 
     project_id   = var.project
     network_name = "miguels-network"
-    routing_mode = "GLOBAL"
 
     subnets = [
         {
